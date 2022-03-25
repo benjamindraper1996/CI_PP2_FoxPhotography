1f41e55ac1e7ec4bf2c36968b50140fc814f79c4
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-25 09:53:15 +0000

    correct cycling of images

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,39 +3,34 @@ var curindex=0
 var orangeBenchPath = "assets/images/orange-bench/"
 var suffix = ".jpg"
 
-var orangebenchArray=new Array()
+var orangeBenchArray=new Array()
 
-	orangebenchArray[0]=`${orangeBenchPath}1${suffix}`
-	orangebenchArray[1]=`${orangeBenchPath}2${suffix}`
-	orangebenchArray[2]=`${orangeBenchPath}3${suffix}`
-	orangebenchArray[3]=`${orangeBenchPath}4${suffix}`
-	orangebenchArray[4]=`${orangeBenchPath}5${suffix}`
-	orangebenchArray[5]=`${orangeBenchPath}6${suffix}`
-    orangebenchArray[6]=`${orangeBenchPath}7${suffix}`
-    orangebenchArray[7]=`${orangeBenchPath}8${suffix}`
-    orangebenchArray[8]=`${orangeBenchPath}9${suffix}`
+	orangeBenchArray[0]=`${orangeBenchPath}1${suffix}`
+	orangeBenchArray[1]=`${orangeBenchPath}2${suffix}`
+	orangeBenchArray[2]=`${orangeBenchPath}3${suffix}`
+	orangeBenchArray[3]=`${orangeBenchPath}4${suffix}`
+	orangeBenchArray[4]=`${orangeBenchPath}5${suffix}`
+	orangeBenchArray[5]=`${orangeBenchPath}6${suffix}`
+    orangeBenchArray[6]=`${orangeBenchPath}7${suffix}`
+    orangeBenchArray[7]=`${orangeBenchPath}8${suffix}`
+    orangeBenchArray[8]=`${orangeBenchPath}9${suffix}`
 
 var preload=new Array()
 
-for (n=0;n<orangebenchArray.length;n++)
+for (n=0;n<orangeBenchArray.length;n++)
 {
 	preload[n]=new Image()
-	preload[n].src=orangebenchArray[n]
+	preload[n].src=orangeBenchArray[n]
 }
 
-document.write('<img name="orangebench" src="'+orangebenchArray[Math.floor(Math.random()*(orangebenchArray.length))]+'">')
+document.write('<img name="orangebench" src="'+orangeBenchArray[Math.floor(Math.random()*(orangeBenchArray.length))]+'">')
 
-function rotateimage()
-{
-
-if (curindex==(tempindex=Math.floor(Math.random()*(orangebenchArray.length)))){
-curindex=curindex==0? 1 : curindex-1
+function rotateimage() {
+	if (curindex==(tempindex=Math.floor(Math.random()*(orangeBenchArray.length)))){
+		curindex=curindex==0? 1 : curindex-1
+	} else {
+		curindex=tempindex
+		document.images.orangebench.src=orangeBenchArray[curindex]
+	}
 }
-else
-curindex=tempindex
-
-	document.images.defaultimage.src=orangebenchArray[curindex]
-}
-
 setInterval("rotateimage()",delay)
-
