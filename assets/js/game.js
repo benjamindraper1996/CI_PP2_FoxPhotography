f8c5f2f6c18415d8aa1856e93aeb85bb3b9e90e7
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-26 18:44:27 +0000

    Add event listener to slides

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
 var game = new Object();
 /* Defines the game board */
-game.tiles = Array.from(document.getElementsByClassName('tile'));
+game.slides = Array.from(document.getElementsByClassName('slide'));
 /* Defines the game folders */
-game.folders = ["bluelight","graffitiHat","graffitiWall","greenscreen","pinkBench","pinkWindows"];
+game.folderList = ["bluelight","graffitiHat","graffitiWall","greenscreen","pinkBench","pinkWindows"];
 game.gameStarted = false;
 /* Sets up the game board */
 game.img = {
@@ -16,3 +16,40 @@ game.img = {
     8: "8.jpg",
     9: "",
 };
+
+game.empty = document.getElementById('emptyslide');
+game.win = Array.from(document.getElementsByClassName('slide'));
+game.gameAreaOuter = document.getElementById('game-area-outer');
+game.gameImgOuter = document.getAnimations('game-img-outer');
+game.imgSelect = document.getElementsByClassName('img-select');
+
+/* function to add images to the slides */
+function setGameImage(folderpath) {
+    if (folderpath == undefined) {
+        var randomFolder = Math.floor((Math.random()*6));
+        var folder = game.folderList[randomFolder]
+        game.gameImgOuter.style.backgroundImage = "url('"+"assets/images/"+folder+"/"+"frame.jpg"+"')"
+
+        for (i=0;i<game.tiles.length; i++) {
+            var slideImage = i + 1;
+            game.slides[i].style.backgroundImage = "url('"+"assets/images/"+folder+"/"+game.img[slideImage]+"')"
+        }
+    } else {
+        game.gameImgOuter.style.backgroundImage = "url('"+"assets/images/"+folderpath+"/"+"frame.jpg"+"')"
+	    for(var i=0;i<game.tiles.length;i++) {
+            var tileImg = i+1;
+		    game.tiles[i].style.backgroundImage = "url('"+"assets/images/"+folderpath+"/"+game.img[slideImage]+"')"
+        }
+    }
+}
+
+/* Adds the event listener for each slide */
+function tileClick () {
+    for (i=0; i<game.slides.length; i++) {
+        game.slides[i].addEventListener('click', function() {
+            shiftSlide(this);
+            winGame();
+        });
+    }
+
+}
