eb0dc43c1c8572fba7cf5b9ff76d245de7b3b094
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-27 11:27:27 +0000

    Add more index.js reworked image arrays

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,23 @@ var pinkWindows = new Array()
 	pinkWindows[0] = "assets/images/pink-Windows/1.jpg";
 	pinkWindows[1] = "assets/images/pink-Windows/2.jpg";
 	pinkWindows[2] = "assets/images/pink-Windows/3.jpg";
+
+var graffitiHat = new Array()
+	graffitiHat[0] = "assets/images/graffiti-hat/1.jpg";
+	graffitiHat[1] = "assets/images/graffiti-hat/2.jpg";
+	graffitiHat[2] = "assets/images/graffiti-hat/3.jpg";
+
+var greenscreen = new Array()
+	greenscreen[0] = "assets/images/greenscreen/1.jpg";
+	greenscreen[1] = "assets/images/greenscreen/2.jpg";
+	greenscreen[2] = "assets/images/greenscreen/3.jpg";
+
+var orangeBench = new Array()
+	orangeBench[0] = "assets/images/orange-bench/1.jpg";
+	orangeBench[1] = "assets/images/orange-bench/2.jpg";
+	orangeBench[2] = "assets/images/orange-bench/3.jpg";
+
+var pinkBench = new Array()
+	pinkBench[0] = "assets/images/pink-bench/1.jpg";
+	pinkBench[1] = "assets/images/pink-bench/2.jpg";
+	pinkBench[2] = "assets/images/pink-bench/3.jpg";
