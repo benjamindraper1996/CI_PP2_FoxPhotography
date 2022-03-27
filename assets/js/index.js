f6d77d4e63953d89fd2ff957c873a1812c157dc5
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-27 11:26:36 +0000

    Add index.js reworked image arrays

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+var delay=1000
+var curindex=0
+
+var blackwhite = new Array()
+	blackwhite[0] = "assets/images/black-white/1.jpg";
+	blackwhite[1] = "assets/images/black-white/2.jpg";
+	blackwhite[2] = "assets/images/black-white/3.jpg";
+
+var graffitiBlueHair = new Array()
+	graffitiBlueHair[0] = "assets/images/graffiti-blue-hair/1.jpg";
+	graffitiBlueHair[1] = "assets/images/graffiti-blue-hair/2.jpg";
+	graffitiBlueHair[2] = "assets/images/graffiti-blue-hair/3.jpg";
+
+var pinkWindows = new Array()
+	pinkWindows[0] = "assets/images/pink-Windows/1.jpg";
+	pinkWindows[1] = "assets/images/pink-Windows/2.jpg";
+	pinkWindows[2] = "assets/images/pink-Windows/3.jpg";
