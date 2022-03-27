75d8910370999e6f1647837d70a70d25f66295ba
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-27 00:12:50 +0000

    added remaining functions to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,7 +20,7 @@ game.img = {
 game.empty = document.getElementById('emptyslide');
 game.win = Array.from(document.getElementsByClassName('slide'));
 game.gameAreaOuter = document.getElementById('game-area-outer');
-game.gameImgOuter = document.getAnimations('game-img-outer');
+game.gameImg = document.getElementById('game-img');
 game.imgSelect = document.getElementsByClassName('img-select');
 
 /* function to add images to the slides */
@@ -28,26 +28,26 @@ function setGameImage(folderpath) {
     if (folderpath == undefined) {
         var randomFolder = Math.floor((Math.random()*6));
         var folder = game.folderList[randomFolder]
-        game.gameImgOuter.style.backgroundImage = "url('"+"assets/images/"+folder+"/"+"frame.jpg"+"')"
+        game.gameImg.style.backgroundImage = "url('"+"assets/images/game/"+folder+"/"+"frame.jpg"+"')"
 
-        for (i=0;i<game.tiles.length; i++) {
+        for (i=0;i<game.slides.length; i++) {
             var slideImage = i + 1;
-            game.slides[i].style.backgroundImage = "url('"+"assets/images/"+folder+"/"+game.img[slideImage]+"')"
+            game.slides[i].style.backgroundImage = "url('"+"assets/images/game/"+folder+"/"+game.img[slideImage]+"')"
         }
     } else {
-        game.gameImgOuter.style.backgroundImage = "url('"+"assets/images/"+folderpath+"/"+"frame.jpg"+"')"
-	    for(var i=0;i<game.tiles.length;i++) {
-            var tileImg = i+1;
-		    game.tiles[i].style.backgroundImage = "url('"+"assets/images/"+folderpath+"/"+game.img[slideImage]+"')"
+        game.gameImg.style.backgroundImage = "url('"+"assets/images/game/"+folderpath+"/"+"frame.jpg"+"')"
+	    for(var i=0;i<game.slides.length;i++) {
+            var slideImage = i + 1;
+		    game.slides[i].style.backgroundImage = "url('"+"assets/images/game/"+folderpath+"/"+game.img[slideImage]+"')"
         }
     }
 }
 
 /* Adds the event listener for each slide */
-function tileClick () {
+function slideClick () {
     for (i=0; i<game.slides.length; i++) {
         game.slides[i].addEventListener('click', function() {
-            shiftSlide(this); // Needs to be set
+            moveSlide(this); // Needs to be set
             winGame(); // Needs to be set
         });
     }
@@ -78,7 +78,7 @@ function shuffleSlides(array) {
     var moves;
 
     for(i=0; i<a; i++) {				
-        var position = Array.prototype.indexOf.call(array,game.blanktile);
+        var position = Array.prototype.indexOf.call(array,game.empty);
 
         if(position == 0){
             moves = topLeft;
@@ -109,9 +109,8 @@ function shuffleSlides(array) {
         }
 
         move = moves[Math.floor(Math.random()*moves.length)];
-        console.log(position);
         array[position] = array[position+move];
-        array[position+move] = game.blanktile;
+        array[position+move] = game.empty;
     }
     return array
 }
@@ -121,22 +120,158 @@ function setSlides(gameobject) {
     var x = gameobject.slides
     x = shuffleSlides(x);
     var set = [[x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7], x[8]]];
-
-    for(i =0; i<3;i++){
+    
+    for (i=0; i<3; i++) {
         position = (Array.prototype.indexOf.call(set[0],set[0][i]))*200
         set[0][i].style.top = "0px";
         set[0][i].style.left = position.toString()+"px";
     }
 
-	for(i =0; i<3;i++){
-        position = (Array.prototype.indexOf.call(set[1],set[1][i]))*200
+	for (i=0; i<3; i++) {
+        position = (Array.prototype.indexOf.call(set[1],set[(1*i)]))*200
 		set[1][i].style.top = "200px";
 		set[1][i].style.left = position.toString()+"px";
     }
 
-	for(i =0; i<3;i++){
+	for (i=0; i<3; i++) {
         position = (Array.prototype.indexOf.call(set[2],set[2][i]))*200
 		set[2][i].style.top = "400px";
 		set[2][i].style.left = position.toString()+"px";
     }
 }
+
+/* function linking to other move functions */
+function moveSlide(slide){
+
+    if (isRight(slide)){
+        moveRight(slide);
+    } else if (isLeft(slide)) {
+        moveLeft(slide);
+    } else if (isTop(slide)){
+        moveTop(slide);
+    } else if (isDown(slide)){
+        moveDown(slide)
+    }
+}
+
+/* Is slide right function */
+function isRight(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+	blank_position = position + 1;
+	if(Object.is(game.slides[blank_position],game.empty) && position!=5 && position!=2 && position!=8){
+        return true;
+    }
+}
+
+/* Move slide right function */
+function moveRight(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    var current_posX = slide.style.left;
+    var res = current_posX.split('px')[0];
+    current = eval(res);
+    slide.style.left = (current+113).toString()+"px";
+    var blank_position = Array.prototype.indexOf.call(game.slides,game.empty);
+    var current_blank_posX = game.empty.style.left;
+    var res_blank = current_blank_posX.split('px')[0];
+    var current_blank = eval(res_blank);
+    game.empty.style.left = (current_blank-113).toString()+"px"; 
+    game.slides[blank_position] = game.slides[position]
+    game.slides[position] = game.empty; 
+}
+
+/* Is slide left function */
+function isLeft(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    blank_position = position - 1;
+    if(Object.is(game.slides[blank_position],game.empty) &&  position!=0 && position!=3 && position!=6){
+        return true;
+    }
+}
+
+/* Move slide left function */
+function moveLeft(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    var current_posX = slide.style.left;
+    var res = current_posX.split('px')[0];
+    current = eval(res);
+    slide.style.left = (current-113).toString()+"px";
+    var blank_position = Array.prototype.indexOf.call(game.slides,game.empty);
+    var current_blank_posX = game.empty.style.left;
+    var res_blank = current_blank_posX.split('px')[0];
+    var current_blank = eval(res_blank);
+    game.empty.style.left = (current_blank+113).toString()+"px";
+    game.slides[blank_position] = game.slides[position]
+    game.slides[position] = game.empty;
+}
+
+/* Is slide top function */
+function isTop(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    blank_position = position-3;
+    if (Object.is(game.slides[blank_position],game.empty)) {
+        return true;
+    }
+}
+
+/* Move slide top function */
+function moveTop(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    var current_posY = slide.style.top;
+    var res = current_posY.split('px')[0];
+    current = eval(res);
+    slide.style.top = (current-113).toString()+"px";
+    var blank_position = Array.prototype.indexOf.call(game.slides,game.empty);
+    var current_blank_posY = game.empty.style.top;
+    var res_blank = current_blank_posY.split('px')[0];
+    var current_blank = eval(res_blank);
+    game.empty.style.top = (current_blank + 113).toString()+"px";
+    game.slides[blank_position] = game.slides[position]
+    game.slides[position] = game.empty;
+}
+
+/* Is slide down function */
+function isDown(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    blank_position = position+3;
+    if (Object.is(game.slides[blank_position],game.empty)) {
+        return true;
+    }
+}
+
+/* Move slide down function */
+function moveDown(slide){
+    var position = Array.prototype.indexOf.call(game.slides,slide);
+    var current_posY = slide.style.top;
+    var res = current_posY.split('px')[0];
+    current = eval(res);
+    slide.style.top = (current+113).toString()+"px";
+    var blank_position = Array.prototype.indexOf.call(game.slides,game.empty);
+    var current_blank_posY = game.empty.style.top;
+    var res_blank = current_blank_posY.split('px')[0];
+    var current_blank = eval(res_blank);
+    game.empty.style.top = (current_blank - 113).toString()+"px";
+    game.slides[blank_position] = game.slides[position]
+    game.slides[position] = game.empty;
+}
+
+function checkBoard(board,winboard){
+    for(i=0; i<board.length; i++){
+        if(board[i]!=winboard[i]){
+            return false
+        } else {
+            return true;
+        }
+    }
+}
+
+function win() {
+    if(isEqual(game.slides,game.win)){
+        setTimeout(function(){ alert("You win!"); }, 500);
+    }
+}
+
+(function (global) {
+    setGameImage();
+	slideClick();
+	setSlides(global);
+}(game));
