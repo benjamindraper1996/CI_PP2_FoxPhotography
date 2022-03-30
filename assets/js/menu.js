fd94f17ba0df0055f080a898cdabd7192665405a
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-30 21:54:23 +0000

    Add menu.js comments

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,18 @@
+/**
+ * The menu.js is used by all html files to display the dropdown navigation 
+ * menu, Social media icons and switch that changes the website between the
+ *  light and dark theme.
+ */
+// Menu Variables
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
+/**
+ * Toggles between showing the dropdown navigation menu and closing it again.
+ */
 function toggleMenu() {
 if (menu.classList.contains("showMenu")) {
   menu.classList.remove("showMenu");
@@ -12,10 +21,13 @@ if (menu.classList.contains("showMenu")) {
 }
 }
 
+//Listens for when the Hamburger icon is clicked.
 hamburger.addEventListener("click", toggleMenu);
 
+//Theme Switcher variable.
 const themeSwitch = document.querySelector('input');
 
+//Listens for when someone clicks on the slider to change the theme.
 themeSwitch.addEventListener('change', () => {
 document.body.classList.toggle('dark-theme');
 });
