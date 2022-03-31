feacb7d31460ffb0b46019d62e3b88f449700f9d
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-31 00:18:37 +0000

    Add about.js comments

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,3 +1,7 @@
+/**
+ * The about.js file is used by the about.html to control the carousel.
+ */
+//carousel variables
 const slider = document.querySelector(".items");
 const slides = document.querySelectorAll(".item");
 const button = document.querySelectorAll(".button");
@@ -5,22 +9,28 @@ let current = 0;
 let prev = 4;
 let next = 1;
 
+//for loop using .length to add in the event listener.
 for (let i = 0; i < button.length; i++) {
 button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
 }
 
+//Previous and Next variables for the lase seen and next images.
 const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+//gotoNum variable for Math to figure out which way to trun teh carousel.
 const gotoNum = number => {
 current = number;
 prev = current - 1;
 next = current + 1;
+
+//For loop removing class' from slides once they are no longer visible to the user.
 for (let i = 0; i < slides.length; i++) {
 	slides[i].classList.remove("active");
 	slides[i].classList.remove("prev");
 	slides[i].classList.remove("next");
 }
 
+//2 if statements to reset next and prev values.
 if (next == 5) {
 	next = 0;
 }
@@ -29,6 +39,7 @@ if (prev == -1) {
 	prev = 4;
 }
 
+//For loop adding class' from slides once they become visible to the user.
 slides[current].classList.add("active");
 slides[prev].classList.add("prev");
 slides[next].classList.add("next");
