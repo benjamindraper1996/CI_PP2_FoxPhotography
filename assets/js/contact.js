6a5f76ac8f499856a633627895b12114d793516f
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-31 21:16:26 +0000

    Add contact.js EmailJS API

---
+++ assets/js/contact.js
@@ -0,0 +1,21 @@
+/**
+ * sendEmail to send email using contact.js, 
+ * The API that is used is described here: https://www.emailjs.com/
+ * @param contactForm [The contact form object]
+ */
+ function sendEmail(contactForm) {
+    emailjs.init("user_gpcsDg9YUsB9BwQGin6JI");
+    emailjs.send("Outlook", "Fox Photography", {
+        "from-name": contactForm.name.value,
+        "from-email": contactForm.email.value,
+        "from-message": contactForm.message-box.value
+    }).then(
+        function (response) {
+            // Email sent successfully.
+        },
+        function (error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // Stop the page from reloading.
+}
