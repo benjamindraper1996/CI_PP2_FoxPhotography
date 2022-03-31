eff7340c6d02fb8e6cc449c054691a8d6cc2850a
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-03-31 23:51:38 +0000

    push changes to contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,19 +3,19 @@
  * The API that is used is described here: https://www.emailjs.com/
  * @param contactForm [The contact form object]
  */
- function sendEmail(contactForm) {
+function sendMail(contactForm) {
     emailjs.init("user_2FuQau4zwU9NqnwDl");
-    emailjs.send("Outlook", "Fox Photography", {
-        "from-name": contactForm.name.value,
-        "from-email": contactForm.email.value,
-        "from-message": contactForm.message-box.value
+    emailjs.send("service_ega18y2", "template_m5hw7tk", {
+        "from_name": contactForm.full_name.value,
+        "from_email": contactForm.email_address.value,
+        "message":  contactForm.message-box.value
     }).then(
         function (response) {
-            // Email sent successfully.
+            console.log("Sending Email", response);
         },
         function (error) {
             console.log("FAILED", error);
         }
     );
-    return false;  // Stop the page from reloading.
+    return false;  // To block from loading a new page
 }
